e5d25733e1aa1f2d152ca9c824b08d5fe8e53fe3
Hassan Ahmed <hassanahmed40005@gmail.com>
2023-04-27 06:36:52 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+
+# Day 1
+
+# What is Programming
+
+# What is Python?
+
+# Features of Python
+
+# What is Python used for
+
+#Day 2
+
+#Python Projects
+#Jarvis Virtual
+#Flappy Bird
+#Snake
+#Love Calculator
+#Face Recognition
+
+##Day 3
+#Module and Pip
+# 1) Built in Moudele . ex : import hashlib
+# 2) External Module  installed through pip ex : pip install pandas
+
+#import pandas
+# Read and work with a file named 'words.csv'
+# df = pandas.read_csv('words.csv')
+# print(df) # This will display first few rows from the words.csv file
+
+#Day 4
+#First Program
+print("Hello World",5,6.89)
+
+
+#Day 5
+#Comments and escape sequence
+
+
