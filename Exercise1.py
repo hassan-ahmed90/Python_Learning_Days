368a7cc1d40fb20c426933831a87eccc8d814eb1
Hassan Ahmed <hassanahmed40005@gmail.com>
2023-04-28 06:54:16 +0500

    Data type , Variables,

---
+++ Exercise1.py
@@ -0,0 +1,19 @@
+#calculator
+
+a=10
+b=2
+sum=a+b
+sub=10-2
+mul=10*2
+exp=2**10
+div=10/2
+divv=10//2
+reminder=10%2
+print("sum of ",a," and ",b," is ",sum)
+print("subtraction of ",a," and ",b," is ",sub)
+print("Multiplication  of ",a," and ",b," is ",mul)
+print("Exponential of ",b," and ",a," is ",exp)
+print("Division of ",a," and ",b," is ",div)
+print("Div without floating of ",a," and ",b," is ",divv)
+print("Reminder of ",a," and ",b," is ",reminder)
+
