644d326d9079f75076ac880f51a4e4401c953e25
Hassan Ahmed <hassanahmed40005@gmail.com>
2023-05-02 07:31:11 +0500

    Loops

--- Day_17.py
+++ Day_17.py
@@ -4,4 +4,43 @@ name="Hassan Ahmed"
 # for i in name:print(i)
 # for i in range(1,10,3):print(i)
 colors=["White","Green","Red",]
-for c in colors : print(c)
+# for c in colors : print(c)
+
+#Day 18
+#convinient
+#emulate
+# i=0
+# while(i<=5):
+#     print(i)
+#     i=i+1
+# a=int(input("Enter the no "))
+# while(a<=28):
+#     a=int(input("No :"))
+#     print(a)
+
+
+#Day 19
+#Break and continious
+
+# i=0
+# while(i<=5):
+#     print(i)
+#     if(i==3):
+#         break
+#     i=i+1
+
+
+
+for i in range(12):
+  if(i == 10):
+    continue
+  print("5 X", i, "=", 5 * i)
+
+  for j in [2, 3, 4, 6, 8, 0]:
+      if (j % 2 != 0):
+          continue
+      print(j)
+
+
+
+
