575993608de653c1570b4fe6e8b1798547d038d3
Hassan Ahmed <hassanahmed40005@gmail.com>
2023-05-02 06:44:09 +0500

    Loops

---
+++ Day_17.py
@@ -0,0 +1,7 @@
+
+#Day 17
+name="Hassan Ahmed"
+# for i in name:print(i)
+# for i in range(1,10,3):print(i)
+colors=["White","Green","Red",]
+for c in colors : print(c)
--- From_Day_11.py
+++ From_Day_11.py
@@ -34,8 +34,20 @@ print(stringMethod.startswith("Cool"))
 
 #Day 14
 
-a=input("Enter the no ")
-if(a>18): print("Greater than 18")
-elif(a==0):print("No is equal to zero")
-elif(a<18):print("Less than 18")
-else:print("Error in Input")
+# a=input("Enter the no ")
+# if(a>18): print("Greater than 18")
+# elif(a==0):print("No is equal to zero")
+# elif(a<18):print("Less than 18")
+# else:print("Error in Input")
+
+#Day 16
+
+ages=input("Enter the age ")
+match ages:
+    case 10:print("age is 10")
+    case 20:print("age is 20")
+    case 30:print("age is 30")
+    case _ if(ages!=40):print(ages," is not 40")
+    case _ if(ages!=50):print(ages," is not 50")
+    case _ : print(ages)
+
