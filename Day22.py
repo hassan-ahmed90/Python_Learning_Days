56cf28283d249fb7ab607073c1de1024e9fbbe4b
Hassan Ahmed <hassanahmed40005@gmail.com>
2023-05-05 18:25:29 +0500

    List

--- Day22.py
+++ Day22.py
@@ -1,5 +1,5 @@
 
-
+#Day 22
 abc=[91,96,35,"Hassan","DSA",True]
 print(abc)
 print(abc[1:])
@@ -9,3 +9,17 @@ if 92 in abc:
     print("Yes it is")
 else:
     print("Nope")
+
+
+    #Day 23
+    #List Method
+    l=[2,4,6,8,10,12,14]
+    l.append(90)
+    l.sort()
+    l.reverse()
+    l.index(8)
+    l.count(10)
+    l.index(2,100)
+    m=[100,200,200]
+    l.extend(m)
+
